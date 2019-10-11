7410f2a074b0c114c3872fff52ca337685fea1c1
duchung07 <duchung97dl@gmail.com>
2019-10-11 15:34:33 +1000

    created react app

---
+++ spotify/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import MessageContainer from "./components/message-container";
+
+function App() {
+  <MessageContainer />;
+}
+
+export default App;
